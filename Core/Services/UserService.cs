0b95633da03a56bfcf4f845f9dddf6c601b846fb
antonia-manolache <94196089+antonia-manolache@users.noreply.github.com>
2023-04-27 02:25:29 +0300

    I did a part of the tasks.

---
+++ Core/Services/UserService.cs
@@ -0,0 +1,60 @@
+﻿using Core.Dtos;
+using DataLayer;
+using DataLayer.Dtos;
+using DataLayer.Entities;
+using DataLayer.Mapping;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.Services
+{
+    public class UserService
+    {
+        private readonly UnitOfWork unitOfWork;
+
+
+        public UserService(UnitOfWork unitOfWork)
+        {
+            this.unitOfWork = unitOfWork;
+        }
+
+        public void Register(RegisterDto registerData)
+        {
+            if (registerData == null)
+            {
+                return;
+            }
+
+
+            var user = new User
+            {
+                Username = registerData.Username,
+                Email = registerData.Email,
+                Password = registerData.Password,
+                RoleId = registerData.RoleId
+            };
+            unitOfWork.Users.Insert(user);
+            unitOfWork.SaveChanges();
+        }
+
+ 
+        public List<User> GetAll()
+        {
+            var results = unitOfWork.Users.GetAll();
+
+            return results;
+        }
+
+        public UserDto GetById(int userId)
+        {
+            var user = unitOfWork.Users.GetById(userId);
+
+            var result = user.ToUserDto();
+
+            return result;
+        }
+    }
+}
---
+++ Project/Controllers/UserController.cs
@@ -0,0 +1,33 @@
+﻿using Core.Dtos;
+using Core.Services;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Project.Controllers
+{
+    [ApiController]
+    [Route("api/users")]
+    [Authorize]
+    public class UserController : ControllerBase
+    {
+        private UserService userService { get; set; }
+        public UserController(UserService userService)
+        {
+            this.userService = userService;
+        }
+
+        [HttpPost("/register")]
+        public IActionResult Register(RegisterDto registerDto)
+        {
+            userService.Register(registerDto);
+            return Ok();
+        }
+
+        [HttpPost("/login")]
+        [AllowAnonymous]
+        public IActionResult Login(LoginDto payload)
+        {
+            return Ok();
+        }
+    }
+}
